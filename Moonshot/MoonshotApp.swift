1f276e9975b00524826b3e79fc14ff4a2c638335
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-15 18:31:25 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mitch Andrade on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
